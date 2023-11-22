cac00d5508fd5d328d7fe7293cd3dbd5ce5180cb
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2023-11-22 20:20:08 +0100

    animation to button added

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
 
 function nextSequence() {
   var randomNummer = Math.floor(Math.random() * 4);
@@ -13,15 +14,29 @@ function nextSequence() {
     .fadeOut(100)
     .fadeIn(100);
 
-  $(".btn").click(function (events) {
-    console.log(events.target);
-    var userChosenColour = events.target.id;
-    var audio = new Audio("./sounds/" + userChosenColour + ".mp3");
-    audio.play();
-  });
-
   gamePattern.push(randomChosenColour);
   console.log(gamePattern);
+}
+
+$(".btn").click(function (events) {
+  var userChosenColour = events.target.id;
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+});
+
+function playSound(name) {
+  var audio = new Audio("./sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function () {
+    $(".btn").removeClass("pressed");
+  }, 100);
 }
 
 nextSequence();
